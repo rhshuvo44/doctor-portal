c33b2de612c94621caa95da628e80b752ef2d9dd
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-05-12 11:40:21 +0600

    footer

--- src/Pages/Share/Footer/Footer.js
+++ src/Pages/Share/Footer/Footer.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import FooterText from './FooterText';
 
 const Footer = () => {
-    return (
-        <div>
-            <p>This is footer</p>
-        </div>
+    return (<>
+        <footer class="footer p-10 m-auto">
+  <div>
+    <span class="footer-title">Services</span> 
+    <a class="link link-hover">Branding</a>
+    <a class="link link-hover">Design</a>
+    <a class="link link-hover">Marketing</a>
+    <a class="link link-hover">Advertisement</a>
+  </div> 
+  <div>
+    <span class="footer-title">Company</span> 
+    <a class="link link-hover">About us</a>
+    <a class="link link-hover">Contact</a>
+    <a class="link link-hover">Jobs</a>
+    <a class="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span class="footer-title">Legal</span> 
+    <a class="link link-hover">Terms of use</a>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </div>
+</footer>
+<FooterText/>
+</>
+
     );
 };
 
---
+++ src/Pages/Share/Footer/FooterText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const footerText = () => {
+    return (
+        <footer class="footer footer-center p-4 text-base-content">
+        <div>
+          <p>Copyright © 2022 - All right reserved</p>
+        </div>
+      </footer>
+    );
+};
+
+export default footerText;
